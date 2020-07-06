832c13c77207a474ace85809b073baae09dcfadf
UKevin94 <kevinung94@gmail.com>
2020-07-06 17:10:33 +0200

    first commit

---
+++ src/test/java/tnr/junitmetalink/RapidfayaTest.java
@@ -0,0 +1,83 @@
+package tnr.junitmetalink;
+
+import org.junit.jupiter.api.*;
+import org.squashtest.ta.galaxia.squash.tf.galaxia.annotations.TFMetadata;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class RapidfayaTest {
+
+    @Test
+    public void test1(){
+        Assertions.assertTrue(true,"Test1 est KO");
+    }
+
+    @Test
+    public void test2(){
+        Assertions.assertTrue(true,"Test2 est KO");
+    }
+
+    @Test
+    public void test3(){
+        Assertions.assertTrue(true,"Test3 est KO");
+    }
+
+    @Test
+    public void test4(){
+        Assertions.assertTrue(true,"Test4 est KO");
+    }
+
+    @Test
+    public void test5(){
+        Assertions.assertTrue(true,"Test5 est KO");
+    }
+
+    @Test
+    public void test6(){
+        Assertions.assertTrue(true,"Test6 est KO");
+    }
+
+    @Test
+    public void test7(){
+        Assertions.assertTrue(true,"Test7 est KO");
+    }
+
+    @Test
+    public void test8(){
+        Assertions.assertTrue(true,"Test8 est KO");
+    }
+
+    @Test
+    public void test9(){
+        Assertions.assertTrue(true,"Test9 est KO");
+    }
+
+    @Test
+    public void test10(){
+        Assertions.assertTrue(false,"Test10 est KO");
+    }
+
+    @Test
+    public void test11() {
+        Assertions.assertTrue(true, "Test11 est KO");
+    }
+    @Test
+    public void test12(){
+        Assertions.assertTrue(true,"Test12 est KO");
+    }
+    @Test
+    public void test13(){
+        Assertions.assertTrue(true,"Test13 est KO");
+    }
+    @Test
+    public void test14(){
+        Assertions.assertTrue(true,"Test14 est KO");
+    }
+    @Test
+    public void test15(){
+        Assertions.assertTrue(true,"Test15 est KO");
+    }
+
+}
